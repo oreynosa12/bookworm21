3b4535d91d6d243b1a9387b05c6d981388b43653
OMR K9 <121472588+oreynosa12@users.noreply.github.com>
2023-06-25 22:06:39 -0400

    Defined query and mutation functionality

--- Develop/server/Schemas/resolvers.js
+++ Develop/server/Schemas/resolvers.js
@@ -1,12 +1,15 @@
 const { User, Book } = require('../models');
 
 const resolvers = {
+    Query: {
+      User: async () => {
+        return await User.find({});
+      },
+      Book: async () => {
+        return await Book.find({});
+      }
+    }
+  };
+  
+  module.exports = resolvers;
 
-}
-
-
-
-
-
-
-module.exports = resolvers;
